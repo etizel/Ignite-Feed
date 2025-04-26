7972023e414be7050b78beb8011e39fdf8513f0d
etiiizel <franciscoetizel@gmail.com>
2025-04-26 16:01:43 -0300

    Btn-LikeCount.feature

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,10 +2,17 @@ import { Trash } from 'phosphor-react';
 import { ThumbsUp } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    return setLikeCount(likeCount + 1);
   }
 
   return (
@@ -39,10 +46,10 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
             {'   '}
-            Aplaudir<span>20</span>
+            Aplaudir<span>{likeCount}</span>
           </button>
         </footer>
       </div>
